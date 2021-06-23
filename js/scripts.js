c1cee9ca2d87e4dc71cb3ce1907503c79e814d52
Ande Caleb <andaeiii@aol.com>
2021-06-23 19:24:49 +0100

    commenting the animation logic...

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 let ball,
     box,
-    bounds,
+    stage,
+    pos = {},
     x = 0,
     y = 0,
     speed = 30,
-    time = 100,
-    vx = vy = 1,        //velocity..
-    intv = null,
-    ballBounds;
+    vx = vy = 5,        //velocity..
+    intv = null;
 
 
 $(document).ready(() => {
     box = $('#box');
     ball = $('#ball');
 
-    getBoxBounds();
-    getBallBounds();
-    intv = setInterval(() => animate(), time);
+    stage = getBoxBounds();
+    b = getBallBounds();        //get ball coordinates..      
+    //intv = setInterval(() => animate(), time);
+    animate();
 });
 
 const noPx = (str) => parseInt(String(str).split('px')[0]);
@@ -24,33 +24,48 @@ const noPx = (str) => parseInt(String(str).split('px')[0]);
 
 const getBoxBounds = () => {
     let { left: x, top: y } = box.offset();
-    bounds = {
+    return {
         x, y,
         'width': noPx(box.css('width')),
         'height': noPx(box.css('height'))
     };
-    console.log(bounds);
 }
 
 const getBallBounds = () => {
+    let { left: x, top: y } = ball.offset();
     let r = parseInt(noPx(ball.css('width'))) / 2;
-
+    return {
+        x, y,
+        'radius': noPx(ball.css('width')),
+        'diameter': noPx(ball.css('height')),
+    };
 }
 
 
 const animate = () => {
 
-    vx = (ball.left > bounds.height) ? -1 : 1;
-    vy = (ball.top > bounds.height) ? -1 : 1;
+    //for the xdirection... 
+    //if the ball's x position - the ball's radius is less than 0 and 
+    // the velocity is less than 0 ~ (reverse the velocity.. )
+    //also if the ball's x position is greater than the stage-Width - the ball radius
+    //and the velocity is greater than 0 reverse the velocity... 
+    if (b.x - b.radius < 0 && vx < 0) vx = -vx;
+    if (b.x + b.radius > stage.width - b.radius && vx > 0) vx = -vx;
 
-    x += speed * vx;
-    y += speed * vy;
+    //for the ydirection... 
+    //repeat the same logic for the y position and the y velocity... 
+    if (b.y - b.radius < 0 && vy < 0) vy = -vy;
+    if (b.y + b.radius > stage.height - b.radius && vy > 0) vy = -vy;
 
-    console.log(x, y);
+    //console.log(bounds, vx, vy);
 
-    gsap.to(ball, 1, { "left": x + "px", "top": y + "px" });
+    //update the position object using the speed times the current velocity. 
+    pos.x = b.x += vx * speed;              //update with velocity..
+    pos.y = b.y += vy * speed;
 
-    // clearInterval(intv);
+    //animate the ball to the new positions... 
+    gsap.to(ball, 0.2, { "left": pos.x + "px", "top": pos.y + "px", onComplete: () => animate() });
+
 }
 
 
