03819d82b4c64310dd736ac0c2985e6160377d7f
Ande Caleb <andaeiii@aol.com>
2021-06-23 16:52:28 +0100

    setting up animations....

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,23 @@ $(document).ready(() => {
     box = $('#box');
     ball = $('#ball');
 
-    get
+    getBoxBounds();
     animate();
 });
+
+const noPx = (str) => parseInt(String(str).split('px')[0]);
+
+
+const getBoxBounds = () => {
+    let b = box.offset();
+    bounds = {
+        ...b,
+        'width': noPx(box.css('width')),
+        'height': noPx(box.css('height'))
+    };
+    console.log(bounds);
+}
+
+const animate = () => {
+    gsap.to(ball, 0.5, { x: 300, y: 400, onComplete });
+}
